c99881bdc2e93bdb6fe45843e82a59f0d23abad2
josegcpa <jose.almeida@research.fchampalimaud.org>
2026-01-12 12:01:10 +0000

    better packaging

---
+++ src/helpful_tools/__init__.py
@@ -0,0 +1,14 @@
+from .tools import *
+
+__all__ = [
+    "read_file",
+    "write_file",
+    "delete_file",
+    "list_files",
+    "search_files",
+    "interpret_image",
+    "read_table",
+    "load_pdf",
+    "web_search",
+    "retrieve_webpage",
+]
